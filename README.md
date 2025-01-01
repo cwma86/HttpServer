5c5c89489d93ffaedb9f440fc72abad22a247e1a
Cory W. Mauer <corymauer@gmail.com>
2024-12-31 20:48:16 -0700

    adding arg parse and very simple readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # HttpServer
 Rust HTTP Server
+
+## Description
+This is a simple rust project to learn the language through implementing a simple web server
+
+## Building
+To build the application navigate to the source code directory from the repo root `./httserver`
+
+Then build using Rust's Cargo utility `cargo build`
+
+## Running
+Run the built application from the built target `proj/HttpServer/httpserver/target/debug/httpserver`
+
+Then from your web browser navigate to localhost:7878
--- httpserver/src/main.rs
+++ httpserver/src/main.rs
@@ -4,12 +4,25 @@ use std::{
     thread,
     time::Duration,
 };
+use clap::Parser;
 
 use httpserver::ThreadPool;
 mod html;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Port number we wish to host the app
+    #[arg(short, long, default_value_t = 7878)]
+    port: u16,
+}
+
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let args = Args::parse();
+    let mut host_port: String = "127.0.0.1:".to_owned();
+    host_port.push_str(&args.port.to_string());
+    println!("hosting website on http://{}", host_port);
+    let listener = TcpListener::bind(host_port).unwrap();
     let pool: ThreadPool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
@@ -24,8 +37,12 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader: BufReader<&TcpStream> = BufReader::new(&stream);
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-
+    let request_line = buf_reader.lines().next();
+    if request_line.is_none(){
+        println!("failed handle");
+        return;
+    }
+    let request_line = request_line.unwrap().unwrap();
 
     let (status_line, filename) = match &request_line[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "/home/corym/proj/HttpServer/httpserver/html/index.html"),
